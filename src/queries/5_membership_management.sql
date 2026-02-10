7931ff919496d006b43ce33c450e93a19f5f0c89
HaseebR67 <haseeb.rehman@ada.ac.uk>
2026-02-10 12:12:46 +0000

    More mistakes corrected

--- src/queries/5_membership_management.sql
+++ src/queries/5_membership_management.sql
@@ -2,7 +2,7 @@
 .mode column
 
 -- 5.1 
-SELECT members.member_id, members.first_name, members.last_name,members.join_date, memberships.type AS memberships_type
+SELECT members.member_id, members.first_name, members.last_name, memberships.type AS membership_type, members.join_date
 FROM memberships 
 INNER JOIN members ON memberships.member_id = members.member_id 
 WHERE memberships.status = 'Active';
